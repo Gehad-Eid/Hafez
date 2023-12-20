7131e7f28b0dc67460ee0a6e264a386113f76883
gehad1995 <gehad.gehad1995@gmail.com>
2023-12-20 11:27:41 +0300

    Initial Commit

---
+++ Hafez/HafezApp.swift
@@ -0,0 +1,17 @@
+//
+//  HafezApp.swift
+//  Hafez
+//
+//  Created by Gehad Eid on 20/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct HafezApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
